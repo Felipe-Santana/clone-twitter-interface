6a05975fe272e497db2e6ec76aee28f82162318e
Felipe-Santana <felipesantanaalbuquerque9@gmail.com>
2020-08-01 12:45:07 -0300

    :lipstick: Add profile page components

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+} from './styles';
 
 const ProfilePage: React.FC = () => {
   return (
     <Container>
-      <Banner></Banner>
+      <Banner>
+        <Avatar />
+      </Banner>
+      <ProfileData>
+        {/* <EditButton outlined>Editar perfil</EditButton> */}
+        <h1>Felipe Santana</h1>
+        <h2>@felipe_santana</h2>
+
+        <p>
+          Developer at <a href=".">@box_delivery</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            São Paulo, Brazil
+          </li>
+          <li>
+            <CakeIcon />
+            Born July 12
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            <strong>345</strong> Following
+          </span>
+          <span>
+            <strong>980</strong> Followers
+          </span>
+        </Followage>
+      </ProfileData>
     </Container>
   );
 };
