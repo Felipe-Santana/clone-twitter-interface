04db4db9cbe92b41d675c8bcd1cad11200867674
Felipe-Santana <felipesantanaalbuquerque9@gmail.com>
2020-12-24 13:31:35 -0300

    :sparkles: add news component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import StickyBox from 'react-sticky-box';
 
 import {
   Container,
@@ -10,6 +11,7 @@ import {
 
 import List from '../List';
 import FollowSuggestion from '../FollowSuggestion';
+import News from '../News';
 
 const SideBar: React.FC = () => {
   return (
@@ -19,16 +21,44 @@ const SideBar: React.FC = () => {
         <SearchIcon />
       </SearchWrapper>
 
-      <Body>
-        <List
-          title="Talvez você curta"
-          elements={[
-            <FollowSuggestion name="Chris Evans" nickname="@ChrisEvans" />,
-            <FollowSuggestion name="Luke Evans" nickname="@TheRealLukevans" />,
-            <FollowSuggestion name="Margot Robbie" nickname="@MargotRobbie" />,
-          ]}
-        />
-      </Body>
+      <StickyBox>
+        <Body>
+          <List
+            title="Pessoas que talvez você conheça"
+            elements={[
+              <FollowSuggestion name="Chris Evans" nickname="@ChrisEvans" />,
+              <FollowSuggestion
+                name="Luke Evans"
+                nickname="@TheRealLukevans"
+              />,
+              <FollowSuggestion
+                name="Margot Robbie"
+                nickname="@MargotRobbie"
+              />,
+            ]}
+          />
+
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+
+          <List
+            title="Talvez você curta"
+            elements={[<News />, <News />, <News />]}
+          />
+        </Body>
+      </StickyBox>
     </Container>
   );
 };
